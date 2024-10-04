19a305af02dc4617412d67786b81d7abec22ee77
gsong <rmsdlf1206@gmail.com>
2024-10-04 16:16:58 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default async function Home() {
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
                 Just the basics - Everything you need to know to set up your
-                database and authentication.
+                database and authentications.
               </div>
             </Link>
             <Link
